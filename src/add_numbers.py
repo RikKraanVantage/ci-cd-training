1aaf524127874b48cd46062ea5044bdc49ad6964
Rik Kraan <rik.kraan@vantage-ai.com>
2021-06-18 11:47:40 +0200

    Refactoring

--- src/add_numbers.py
+++ src/add_numbers.py
@@ -1,2 +1,5 @@
-def add_numbers(a, b):
-    return a + b
+"""Contains function to add two numbers"""
+
+def add_numbers(num_1, num_2):
+    """Adds numbers"""
+    return num_1 + num_2
--- src/subtract_numbers.py
+++ src/subtract_numbers.py
@@ -1,2 +1,7 @@
-def subtract_numbers(a, b):
-    return a - b
+"""Contains function for subtracting numbers"""
+
+
+def subtract_numbers(num_1, num_2):
+    """Subtracts num_2 from num_1"""
+    return num_1 - num_2
+
